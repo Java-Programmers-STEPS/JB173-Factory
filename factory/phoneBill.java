3a3ee690c1f05e00316d2cc0179c2c434455f477
Java-Programmers-STEPS <121227713+Java-Programmers-STEPS@users.noreply.github.com>
2023-02-17 09:35:04 +0530

    Update phoneBill.java

--- factory/phoneBill.java
+++ factory/phoneBill.java
@@ -1,14 +1,15 @@
 package factory;
 import java.io.*;
+import java.util.Scanner;
  
 public class phoneBill {
       public static void main(String args[])throws IOException{
             SelectNetworkFactory planFactory = new SelectNetworkFactory();
             System.out.print("Enter the name of network for which the bill will be generated: ");
-            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-            String networkName=br.readLine();
+           Scanner scanner=new Scanner(System.in);
+            String networkName=scanner.next();
             System.out.print("Enter the number of minutes for bill will be calculated: ");
-            int minutes=Integer.parseInt(br.readLine());
+            int minutes=scanner.nextInt();
             cellularplan p = planFactory.getPlan(networkName);
             System.out.print("Bill amount for "+networkName+" of "+minutes+" units is: ");
             p.getRate();
